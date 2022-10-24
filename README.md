47e2eedc225309d6be876083944e942e628039ab
ArthurDias96 <65253786+ArthurDias96@users.noreply.github.com>
2022-10-24 19:00:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1 align="center">
+FISCHER CONSTRUÇÕES
+ </h1>
+ <h2 align="left">
+Este é o meu primeiro projeto para minha empresa. Criado com muito carinho e cuidadosamente, usando dos mais refinados temperos que a programação nos oferece!</h2>
+<h4 align="left"> 
+	🚧 🚀 Em construção... 🚀 🚧
+</h4>
+<h5 align="left">
+Fischer Construções construído com:
+PHP 
+HTML
+CSS
+JAVASCRIPT
+</h5>
---
+++ enviar.php
@@ -0,0 +1,46 @@
+<?php
+date_defaut_timezone_set('America/Sao_Paulo');
+
+require_once('src/PHPMailer.php');
+require_once('src/SMTP.php');
+require_once('src/Exception.php');
+
+use PHPMailer\PHPMailer;
+use PHPMailer\SMTP;
+use PHPMailer\Exception;
+
+if((isset($_POST['email']) && !empty(trim($_POST['email']))) && (isset($_POST['mensagem']) && !empty(trim($_POST['mensagem'])))){
+	
+	$nome = !empty($_POST['nome']) ? $_POST['nome'] : 'Não informado';
+	$email = !empty($_POST['email']); 
+	$assunto = !empty($_POST['assunto']); ? utf8_decode($_POST['assunto']) : 'Não informado';
+	$mensagem = !empty($_POST['mensagem']);
+	$data = date('d/m/Y H:i:s');
+
+	$mail = new PHPMailer();
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$SMTPAuth = true;
+	$mail->Username = 'seuemail@gmail.com';
+	$mail->Passaword = 'senhadoemail';
+	$mail->Port = 587;
+	$mail->setFrom('seuemail@gmail.com');
+	$mail->addAddress('endereco1@provedor.com.br');
+	$mail->isHTML(true);
+	$mail->Subject = $assunto;
+	$mail->body = "Nome: {$nome}<br>
+	 			   Email: {$email}<br>
+				   Mensagem: {$mensage}<br>
+				   Data/hora: {$data}";
+
+if($mail->send()) {
+	echo'Email enviado com sucesso.';
+} else{
+	echo'Email nao enviado.';
+}
+} else{
+	echo 'Não enviado: informar o email e a mensagem.';
+
+}
+
+
